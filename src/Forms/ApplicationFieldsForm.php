1ea6a09f0eef2c7f2f8c128a05cb929b98acddb0
ItsKevinRafaell <itskevin.rafael@gmail.com>
2025-02-14 14:40:09 +0700

    fix:layout application fields form

--- src/Forms/ApplicationFieldsForm.php
+++ src/Forms/ApplicationFieldsForm.php
@@ -42,12 +42,16 @@ class ApplicationFieldsForm
             ])
                 ->columns(4)
                 ->visible(fn () => config('filament-general-settings.show_logo_and_favicon')),
-            TextInput::make('support_email')
+            Grid::make()->schema([
+                TextInput::make('support_email')
                 ->label(__('filament-general-settings::default.support_email'))
                 ->prefixIcon('heroicon-o-envelope'),
-            TextInput::make('support_phone')
-                ->prefixIcon('heroicon-o-phone')
-                ->label(__('filament-general-settings::default.support_phone')),
+                TextInput::make('support_phone')
+                    ->prefixIcon('heroicon-o-phone')
+                    ->label(__('filament-general-settings::default.support_phone')),
+            ])
+                ->columns(4)
+                ->visible(fn () => config('filament-general-settings.show_support_contact')),
             // ColorPicker::make('theme_color')
             //     ->label(__('filament-general-settings::default.theme_color'))
             //     ->prefixIcon('heroicon-o-swatch')
